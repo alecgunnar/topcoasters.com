2ced9d22ebcfafafdeb53418c99b62912e999921
gunnar94 <gunnar94@me.com>
2014-02-17 18:09:58 -0500

    Added testing config

---
+++ Config/Test/Database.php
@@ -0,0 +1,6 @@
+<?php
+
+return array('mysql' => array('default' => array('username' => 'topcoaster_test',
+                                                 'password' => 'eYHDBXEhphu6csp4',
+                                                 'host'     => 'localhost',
+                                                 'name'     => 'topcoaster_test')));
---
+++ Config/Test/System.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * General system configuration
+ */
+
+return array('site_name'    => 'Top Coasters',
+             'description'  => 'Experience the top thrill!',
+             'admin_name'   => 'Administrator',
+             'admin_email'  => 'topcoasters@gmail.com',
+             'auto_route'   => true,
+             'timezone'     => 'US/Eastern',
+             'url'          => 'http://test.topcoasters.com/');
