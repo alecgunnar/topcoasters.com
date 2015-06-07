d529f3f36c6c672f1351382de4da59a0b33eacb0
gunnar94 <gunnar94@me.com>
2015-06-06 20:33:07 -0400

    Started the funny error pages :P

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  match '/404', to: 'errors#file_not_found', via: :all
+	match '/422', to: 'errors#unprocessable', via: :all
+	match '/500', to: 'errors#internal_server_error', via: :all
+
   root 'welcome#index'
+
+  if Rails.env.development?
+  	get '/error/not-found', to:'errors#file_not_found'
+  	get '/error/server', to:'errors#internal_server_error'
+  	get '/error/unprocessable', to:'errors#unprocessable'
+ 	end
 end
