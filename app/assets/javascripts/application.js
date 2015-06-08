78f5b071c824c1d3d569b78d348fe9444f0527bc
gunnar94 <gunnar94@me.com>
2015-06-08 18:32:38 -0400

    Converted indentation to spaces

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,31 +4,31 @@
 //= require_tree .
 
 var mobileNavTogglers   = '.header__main-nav-drop, .header__member-nav-drop',
-		mobileNavOpen       = false,
-		whichMobileNav      = null,
-		mobileNavContainers = {
-			main:   '.nav__main',
-			member: '.header__member-nav'
-		};
+    mobileNavOpen       = false,
+    whichMobileNav      = null,
+    mobileNavContainers = {
+      main:   '.nav__main',
+      member: '.header__member-nav'
+    };
 
 $(document).ready(function() {
-	$(mobileNavTogglers).on('click', function() {
-		var $this    = $(this),
-				navClass = mobileNavContainers[$this.attr('data-nav')];
+  $(mobileNavTogglers).on('click', function() {
+    var $this    = $(this),
+        navClass = mobileNavContainers[$this.attr('data-nav')];
 
-		if(mobileNavOpen) {
-			if(navClass != whichMobileNav)
-				return;
+    if(mobileNavOpen) {
+      if(navClass != whichMobileNav)
+        return;
 
-			$(navClass).slideUp(function() {
-				mobileNavOpen  = false;
-				whichMobileNav = null;
-			});
-		} else {
-			$(navClass).slideDown(function() {
-				mobileNavOpen  = true;
-				whichMobileNav = navClass;
-			});
-		}
-	});
+      $(navClass).slideUp(function() {
+        mobileNavOpen  = false;
+        whichMobileNav = null;
+      });
+    } else {
+      $(navClass).slideDown(function() {
+        mobileNavOpen  = true;
+        whichMobileNav = navClass;
+      });
+    }
+  });
 });
